eea0a4d5ecbb36210a7f899a4521a2651abab41d
Kenneth Lai <kennethlai.ny@gmail.com>
2024-12-11 15:13:55 -0500

    add colors to coordinates

--- src/components/MouseTracker/MouseTracker.tsx
+++ src/components/MouseTracker/MouseTracker.tsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 export const MouseTracker = (): JSX.Element => {
   const [coordinates, setCoordinates] = useState<[number, number]>([0, 0]);
   const [x, y] = coordinates;
+  const [isLargerX, setIsLargerX] = useState<boolean>(false);
 
   useEffect(() => {
     const getMouseCoordinates = (event: MouseEvent): void => {
       const x = event.clientX;
       const y = event.clientY;
       setCoordinates([x, y]);
+      setIsLargerX(x > y);
     };
 
     document.addEventListener("mousemove", getMouseCoordinates);
@@ -20,7 +22,17 @@ export const MouseTracker = (): JSX.Element => {
     <>
       <div className="title">Mouse coordinates</div>
       <div>
-        {x}, {y}
+        <span
+          className={isLargerX ? "larger-coordinate" : "smaller-coordinate"}
+        >
+          {x}
+        </span>
+        ,{" "}
+        <span
+          className={isLargerX ? "smaller-coordinate" : "larger-coordinate"}
+        >
+          {y}
+        </span>
       </div>
     </>
   );
